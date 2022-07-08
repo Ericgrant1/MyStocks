58de7af8adfd884ba90a29521a4591d7e35fce7b
Eric Grant <senatorericgrant@gmail.com>
2022-07-08 23:59:44 +0700

    feat: add search controller

--- MyStocks/Controllers/WatchListViewController.swift
+++ MyStocks/Controllers/WatchListViewController.swift
@@ -12,8 +12,31 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
     }
 
+    private func setUpSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+                  return
+              }
+        
+        // Optimise to reduce number of searches for when user stops typing
+        
+        // Call API to search
+        
+        // Update results controller
+        
+    }
+}
