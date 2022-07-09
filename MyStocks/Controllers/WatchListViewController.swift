dacca493682112a6dab7f343fa308bd9b2ff783f
Eric Grant <senatorericgrant@gmail.com>
2022-07-09 22:38:55 +0700

    feat: add custom title view

--- MyStocks/Controllers/WatchListViewController.swift
+++ MyStocks/Controllers/WatchListViewController.swift
@@ -8,20 +8,46 @@
 import UIKit
 
 class WatchListViewController: UIViewController {
-
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setUpSearchController()
+        setUpTitleView()
     }
 
+    // MARK: - Private
+    
+    private func setUpTitleView() {
+        let titleView = UIView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: view.width,
+                height: navigationController?.navigationBar.height ?? 100)
+        )
+        
+        let label = UILabel(
+            frame: CGRect(
+                x: 10,
+                y: 0,
+                width: titleView.width - 20,
+                height: titleView.height)
+        )
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 40, weight: .medium)
+        titleView.addSubview(label)
+        navigationItem.titleView = titleView
+    }
+    
     private func setUpSearchController() {
         let resultVC = SearchResultsViewController()
         let searchVC = UISearchController(searchResultsController: resultVC)
         searchVC.searchResultsUpdater = self
         navigationItem.searchController = searchVC
     }
-
 }
 
 extension WatchListViewController: UISearchResultsUpdating {
