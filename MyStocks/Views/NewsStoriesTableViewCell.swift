76dd8d03bd716147e547ab9d2b0d7948f8c205fa
Eric Grant <senatorericgrant@gmail.com>
2022-07-19 23:42:37 +0700

    feat: add elements for custom cell

--- MyStocks/Views/NewsStoriesTableViewCell.swift
+++ MyStocks/Views/NewsStoriesTableViewCell.swift
@@ -14,6 +14,37 @@ class NewsStoriesTableViewCell: UITableViewCell {
         
     }
     
+    // Source
+    private let sourceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        return label
+    }()
+    
+    // Headline
+    private let headlineLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        return label
+    }()
+    
+    // Date
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .light)
+        return label
+    }()
+    
+    // Image
+    private let storyImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 6
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
