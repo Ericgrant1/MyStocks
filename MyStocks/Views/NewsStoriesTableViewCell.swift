818c207207809dcda0c79cc9eab93ca2cb0a831c
Eric Grant <senatorericgrant@gmail.com>
2022-07-20 22:00:44 +0700

    feat: display the labels layout of the cell

--- MyStocks/Views/NewsStoriesTableViewCell.swift
+++ MyStocks/Views/NewsStoriesTableViewCell.swift
@@ -36,13 +36,15 @@ class NewsStoriesTableViewCell: UITableViewCell {
     // Headline
     private let headlineLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.font = .systemFont(ofSize: 24, weight: .regular)
+        label.numberOfLines = 0
         return label
     }()
     
     // Date
     private let dateLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 14, weight: .light)
         return label
     }()
@@ -50,7 +52,7 @@ class NewsStoriesTableViewCell: UITableViewCell {
     // Image
     private let storyImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .white
+        imageView.backgroundColor = .tertiarySystemBackground
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
         imageView.layer.cornerRadius = 6
@@ -60,8 +62,8 @@ class NewsStoriesTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .systemPink
-        backgroundColor = .systemPink
+        contentView.backgroundColor = .secondarySystemBackground
+        backgroundColor = .secondarySystemBackground
         addSubviews(sourceLabel, headlineLabel, dateLabel, storyImageView)
     }
     
@@ -73,12 +75,36 @@ class NewsStoriesTableViewCell: UITableViewCell {
         super.layoutSubviews()
         
         let imageSize: CGFloat = contentView.height - 6
-        storyImageView.frame = CGRect(x: contentView.width - imageSize - 10,
-                                      y: 3,
-                                      width: imageSize,
-                                      height: imageSize)
+        storyImageView.frame = CGRect(
+            x: contentView.width - imageSize - 10,
+            y: 3,
+            width: imageSize,
+            height: imageSize
+        )
         
         // Layout labels
+        let availableWidth: CGFloat = contentView.width - separatorInset.left - imageSize - 15
+        dateLabel.frame = CGRect(
+            x: separatorInset.left,
+            y: contentView.height - 40,
+            width: availableWidth,
+            height: 40
+        )
+        
+        sourceLabel.sizeToFit()
+        sourceLabel.frame = CGRect(
+            x: separatorInset.left,
+            y: 4,
+            width: availableWidth,
+            height: sourceLabel.height
+        )
+        
+        headlineLabel.frame = CGRect(
+            x: separatorInset.left,
+            y: sourceLabel.bottom + 5,
+            width: availableWidth,
+            height: contentView.height - sourceLabel.bottom - dateLabel.height - 10
+        )
     }
     
     override func prepareForReuse() {
