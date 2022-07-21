e2b0e3ce65675c3ee7a2ab7e1a88212d944f6050
Eric Grant <senatorericgrant@gmail.com>
2022-07-21 23:39:19 +0700

    fix: improve size layout items

--- MyStocks/Views/NewsStoriesTableViewCell.swift
+++ MyStocks/Views/NewsStoriesTableViewCell.swift
@@ -37,7 +37,7 @@ class NewsStoriesTableViewCell: UITableViewCell {
     // Headline
     private let headlineLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 24, weight: .regular)
+        label.font = .systemFont(ofSize: 22, weight: .regular)
         label.numberOfLines = 0
         return label
     }()
@@ -75,10 +75,10 @@ class NewsStoriesTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let imageSize: CGFloat = contentView.height - 6
+        let imageSize: CGFloat = contentView.height / 1.4
         storyImageView.frame = CGRect(
             x: contentView.width - imageSize - 10,
-            y: 3,
+            y: (contentView.height - imageSize) / 2,
             width: imageSize,
             height: imageSize
         )
