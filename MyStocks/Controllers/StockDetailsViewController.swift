52a619ea7fa96e2c136c9772069f6cbf6c4ce860
Eric Grant <senatorericgrant@gmail.com>
2022-07-28 00:23:06 +0700

    feat: add watchlist interaction

--- MyStocks/Controllers/StockDetailsViewController.swift
+++ MyStocks/Controllers/StockDetailsViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class StockDetailsViewController: UIViewController {
+    // MARK: - Properties
     
-    // Symbol, Company Name, Any chart data we may have
-
+    private let symbol: String
+    private let companyName: String
+    private let candleStickData: [CandleStick]
+    
+    // MARK: - Init
+    
+    init(
+        symbol: String,
+        companyName: String,
+        candleStickData: [CandleStick] = []
+    ) {
+        self.symbol = symbol
+        self.companyName = companyName
+        self.candleStickData = candleStickData
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
